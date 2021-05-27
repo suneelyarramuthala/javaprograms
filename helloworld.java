ddb80cf9dfdc11b12dab0e1d1787e7345f67ac59
suneelyarramuthala <70442430+suneelyarramuthala@users.noreply.github.com>
2021-05-27 19:15:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
 
-class CheckEvenOdd
-{
-  public static void main(String args[])
-  {
-    int num;
-    System.out.println("Enter an Integer number:");
 
-    Scanner input = new Scanner(System.in);
-    num = input.nextInt();
-
-    if ( num % 2 == 0 )
-        System.out.println("Entered number is even");
-     else
-        System.out.println("Entered number is odd");
-  }
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
